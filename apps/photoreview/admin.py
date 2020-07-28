3056fc6fe09f196128bb8e53a4ac584170ceb0a8
Amy Ghotra <amy.ghotra8@gmail.com>
2020-07-27 20:01:14 -0400

    register with profile pic works

--- apps/photoreview/admin.py
+++ apps/photoreview/admin.py
@@ -2,21 +2,13 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
-<<<<<<< Updated upstream
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-=======
-from .forms import CustomUserChangeForm, CustomUserCreationForm
->>>>>>> Stashed changes
 from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-<<<<<<< Updated upstream
     list_display = ['email', 'username',]
-=======
-    list_display = ['username','email']
->>>>>>> Stashed changes
 
 admin.site.register(CustomUser, CustomUserAdmin)
