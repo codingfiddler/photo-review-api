aa1ec179633d272c6a81f4c231216fa3aedf2ef6
Amy Ghotra <amy.ghotra8@gmail.com>
2020-07-28 20:37:04 -0400

    able to fetch all images from s3

---
+++ apps/photoreview/bucketAccess.py
@@ -0,0 +1,7 @@
+# import boto3
+
+# s3 = boto3.resource('s3')
+# bucket = s3.Bucket('krino-photos')
+# startAfter = 'krino-photos'
+# for obj in bucket.objects.all():
+#     print(obj.key) # key is the "file name"
--- apps/photoreview/models.py
+++ apps/photoreview/models.py
@@ -55,3 +55,6 @@ class UploadedPhoto(models.Model):
     #         'camera': exifgetter('Model'),
     #     },
     # )
+
+# class AllImages(models.Model):
+    
